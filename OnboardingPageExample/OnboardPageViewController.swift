892e5723bfe40f8ef585bd5f173df6e949b91ed2
syeonee <syeoneee326@gmail.com>
2023-09-25 23:55:47 +0900

    feat: Indicator를 통한 온보딩 페이지 이동 구현

---
+++ OnboardingPageExample/OnboardPageViewController.swift
@@ -0,0 +1,76 @@
+//
+//  OnboardPageViewController.swift
+//  OnboardingPageExample
+//
+//  Created by 김시연 on 2023/09/18.
+//
+
+import UIKit
+
+class OnboardPageViewController: UIPageViewController {
+    
+    var contentPageViewControllerList = [UIViewController]()
+    weak var onboardDelegate: OnboardPageControlDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataSource = self
+        self.delegate = self
+
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        
+        contentPageViewControllerList = [
+            storyBoard.instantiateViewController(withIdentifier: "OneOnboard"),
+            storyBoard.instantiateViewController(withIdentifier: "TwoOnboard"),
+            storyBoard.instantiateViewController(withIdentifier: "ThreeOnboard"),
+            storyBoard.instantiateViewController(withIdentifier: "FourOnboard"),
+            storyBoard.instantiateViewController(withIdentifier: "FiveOnboard")
+        ]
+        onboardDelegate?.numberOfPage(numberOfPage: contentPageViewControllerList.count)
+        setViewControllers([contentPageViewControllerList[0]], direction: .forward, animated: false, completion: nil)
+    }
+    
+    
+    func goToPage(index: Int) {
+        let currentViewController = viewControllers!.first!
+        let currentViewControllerIndex = contentPageViewControllerList.firstIndex(of: currentViewController)!
+        
+        let direction: NavigationDirection = index > currentViewControllerIndex ? .forward : .reverse
+        setViewControllers([contentPageViewControllerList[index]], direction: direction, animated: false, completion: nil)
+    }
+    
+
+}
+
+extension OnboardPageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let currentIndex = contentPageViewControllerList.firstIndex(of: viewController)!
+        if currentIndex == 0 {
+            return nil
+        } else {
+            return contentPageViewControllerList[currentIndex - 1]
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let currentIndex = contentPageViewControllerList.firstIndex(of: viewController)!
+        if currentIndex == contentPageViewControllerList.count - 1 {
+            return nil
+        } else {
+            return contentPageViewControllerList[currentIndex + 1]
+        }
+    }
+    
+}
+
+extension OnboardPageViewController: UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if let currentPageViewController = pageViewController.viewControllers?.first {
+            let index = contentPageViewControllerList.firstIndex(of: currentPageViewController)!
+            onboardDelegate?.pageChangedTo(index: index)
+            
+        }
+    }
+    
+}
--- OnboardingPageExample/ViewController.swift
+++ OnboardingPageExample/ViewController.swift
@@ -8,12 +8,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var onboardPageControl: UIPageControl!
+    
+    var onboardPageViewController: OnboardPageViewController!
+    var currentIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        onboardPageControl.preferredIndicatorImage = UIImage(named: "otherPageIndicator")
+        let startPage = 0  // Assume the 1st page is the start page
+        onboardPageControl.setIndicatorImage(UIImage(named: "currentPageIndicator"), forPage: startPage)
+        onboardPageControl.pageIndicatorTintColor = UIColor(named: "Gray")
+        onboardPageControl.currentPageIndicatorTintColor = UIColor(named: "Neo")
+        
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let desinationViewController = segue.destination as? OnboardPageViewController {
+            onboardPageViewController = desinationViewController
+            onboardPageViewController.onboardDelegate = self
+        }
+    }
+    
+    @IBAction func pageControlValueChanged(_ sender: Any) {
+        let currentPageIndex = onboardPageControl.currentPage
+        currentIndex = currentPageIndex
+        onboardPageViewController.goToPage(index: currentPageIndex)
+        updatePageControlUI(currentPageIndex: onboardPageControl.currentPage)
+    }
+    
+    func updatePageControlUI(currentPageIndex: Int) {
+        onboardPageControl.pageIndicatorTintColor = UIColor(named: "Gray")
+        onboardPageControl.currentPageIndicatorTintColor = UIColor(named: "Neo")
+        
+        (0..<onboardPageControl.numberOfPages).forEach { (index) in
+            let currentPageIconImage = UIImage(named: "currentPageIndicator")
+            let otherPageIconImage = UIImage(named: "otherPageIndicator")
+            let pageIcon = index == currentPageIndex ? currentPageIconImage : otherPageIconImage
+            onboardPageControl.setIndicatorImage(pageIcon, forPage: index)
+            
+        }
     }
 
+}
 
+extension ViewController: OnboardPageControlDelegate {
+    func numberOfPage(numberOfPage: Int) {
+        onboardPageControl.numberOfPages = numberOfPage
+    }
+    
+    func pageChangedTo(index: Int) {
+        updatePageControlUI(currentPageIndex: index)
+        onboardPageControl.currentPage = index
+        currentIndex = index
+
+    }
+    
+    
+}
+
+protocol OnboardPageControlDelegate: AnyObject {
+    func numberOfPage(numberOfPage: Int)
+    func pageChangedTo(index: Int)
 }
 
